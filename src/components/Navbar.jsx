68e83585c69d579bb75dff83b8a1d3fd836ab2bf
Xuan Sang <nguyenxuansang10032001@gmail.com>
2025-06-06 09:13:46 +0700

    fix-xong-v5

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -204,12 +204,12 @@ export default function Navbar() {
       <div className='lg:hidden'>
         {isOpenMenu ? (
           <IoMdClose
-            className='text-[40px] text-primary bg-white z-10 cursor-pointer p-2 rounded-md'
+            className='text-[40px] text-white z-10 cursor-pointer rounded-md'
             onClick={() => setIsOpenMenu(false)}
           />
         ) : (
           <IoMenuSharp
-            className='text-[40px] text-primary bg-white z-10 cursor-pointer p-2 rounded-md'
+            className='text-[40px] text-white z-10 cursor-pointer rounded-md'
             onClick={() => setIsOpenMenu(true)}
           />
         )}
